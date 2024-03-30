258d826195ab69bec5dd4f7ae21cc0de669f42c4
Yong Cheng Low <lowyongcheng@hotmail.com>
2024-03-30 12:13:09 +0800

    feat(user): Add custom devise controllers, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+               confirmations: "users/confirmations",
+               passwords: "users/passwords",
+               registrations: "users/registrations",
+               sessions: "users/sessions",
+               unlocks: "users/unlocks",
+             },
+             path_names: {
+               sign_in: "login", # Use user/login instead of sign_in
+             }
+
+  devise_scope :user do
+    get "users/:id", to: "users/registrations#show", as: :user
+  end
+
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
